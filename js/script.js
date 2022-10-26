46821a758bb0977f0c1df97c18ccc15b83f95c94
VitMach <machihin_vitalii@mail.ru>
2022-10-26 23:01:46 +0300

    bagfix по замечаниям ревьюера v1.1

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
+let nameUser = document.querySelector('.info__title');
+let aboutMe = document.querySelector('.info__subtitle');
 let editInfoBtn = document.querySelector('.info__editing-btn');
 let popup = document.querySelector('.popup');
-let closePopup = document.querySelector('.popup__close');
+let closePopupBtn = document.querySelector('.popup__close');
 let formInfo = document.querySelector('.popup__container');
 let nameInput = document.querySelector('#name');
 let aboutMeInput = document.querySelector('#aboutMe');
 
 // Открытие попапа
-editInfoBtn.addEventListener('click', function showPopup() {
-	popup.classList.add('popup_opened')
-})
+function showPopup() {
+	nameInput.value = nameUser.textContent;
+	aboutMeInput.value = aboutMe.textContent;
+	popup.classList.add('popup_opened');
+}
 // Закрытие попапа
-closePopup.addEventListener('click', function closePopup() {
+function closePopup() {
 	popup.classList.remove('popup_opened')
-})
+}
 
 function editInfoHandler(evt) {
 	evt.preventDefault();
-
-	let nameUser = document.querySelector('.info__title');
-	let aboutMe = document.querySelector('.info__subtitle');
 
 	nameUser.textContent = nameInput.value;
 	aboutMe.textContent = aboutMeInput.value;
@@ -26,4 +27,6 @@ function editInfoHandler(evt) {
 	popup.classList.remove('popup_opened')
 }
 
+editInfoBtn.addEventListener('click', showPopup);
+closePopupBtn.addEventListener('click', closePopup);
 formInfo.addEventListener('submit', editInfoHandler); 
