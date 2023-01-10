dde605e124be45ffc2ae08c24932bcfd598e2df9
VitMach <machihin_vitalii@mail.ru>
2023-01-10 21:15:18 +0300

    BagFix v0.2 после замечаний ревьюера

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,18 +4,20 @@ export default class Popup {
   constructor(selector) {
     this._selector = selector;
     this._buttonKey = 'Escape';
+    this._handleEscClose = this._handleEscClose.bind(this);/* вручную привязываем функцию к this, 
+    что бы корректно добавлялся и удалялся обработчик */
   }
 
   open() {
     this._selector.classList.add('popup_opened');
 
-    document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._selector.classList.remove('popup_opened');
 
-    document.removeEventListener('keydown', (evt) => this._handleEscClose(evt));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
